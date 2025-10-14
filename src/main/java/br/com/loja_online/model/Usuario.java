b2f751fefd23490ddecaf958fe6606278283f5eb
Renato Druzian <rendruzian@gmail.com>
2025-10-14 14:12:58 -0300

    criado camadas para finalizar api

--- src/main/java/br/com/loja_online/model/Usuario.java
+++ src/main/java/br/com/loja_online/model/Usuario.java
@@ -1,13 +1,18 @@
 package br.com.loja_online.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name="tb_usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 public class Usuario{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,100 +24,10 @@ public class Usuario{
     private String cpf;
     private String dataNAscimento;
     private String genero;
+    @OneToMany(mappedBy = "id_usuario", cascade = CascadeType.ALL)
     private Cartao cartao;
+    @OneToMany(mappedBy = "id_usuario", cascade = CascadeType.ALL)
     private Endereco endereco;
     private String foto;
-
-    public Usuario(String nome, String login, String telefone, String email, String cpf, String dataNAscimento, String genero, Cartao cartao, Endereco endereco, String foto) {
-        this.nome = nome;
-        this.login = login;
-        this.telefone = telefone;
-        this.email = email;
-        this.cpf = cpf;
-        this.dataNAscimento = dataNAscimento;
-        this.genero = genero;
-        this.cartao = cartao;
-        this.endereco = endereco;
-        this.foto = foto;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getDataNAscimento() {
-        return dataNAscimento;
-    }
-
-    public void setDataNAscimento(String dataNAscimento) {
-        this.dataNAscimento = dataNAscimento;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public Cartao getCartao() {
-        return cartao;
-    }
-
-    public void setCartao(Cartao cartao) {
-        this.cartao = cartao;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getFoto() {
-        return foto;
-    }
-
-    public void setFoto(String foto) {
-        this.foto = foto;
-    }
+    private String tipo;
 }
