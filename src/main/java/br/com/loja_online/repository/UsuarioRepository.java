beb63c157dd30ed8412fb12d382af383d41f5c87
Renato Druzian <rendruzian@gmail.com>
2025-10-20 20:49:20 -0300

    save

--- src/main/java/br/com/loja_online/repository/UsuarioRepository.java
+++ src/main/java/br/com/loja_online/repository/UsuarioRepository.java
@@ -3,6 +3,9 @@ package br.com.loja_online.repository;
 import br.com.loja_online.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {}
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByLogin(String login);
+}
--- src/main/java/br/com/loja_online/service/UsuarioService.java
+++ src/main/java/br/com/loja_online/service/UsuarioService.java
@@ -9,9 +9,22 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
-    public Usuario findByLogin(String login){}
-    public Usuario findById(Integer id){}
-    public void deleteById(Integer id){}
-    public void criaUsuario(Usuario usuario){}
-    public void atualizaUsuario(Usuario usuario){}
+    public Usuario findByLogin(String login){
+        return usuarioRepository.findByLogin(login).orElseThrow(() -> new IllegalArgumentException("Login não encontrado"));
+    }
+    public Usuario findById(Integer id){
+        return usuarioRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado"));
+    }
+    public void deleteById(Integer id){
+        usuarioRepository.deleteById(id);
+    }
+    public void criaUsuario(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+    public void atualizaUsuario(Usuario usuario){
+        if (usuario.getId() == null || !usuarioRepository.existsById(usuario.getId())) {
+            throw new IllegalArgumentException("Usuário não encontrado para atualizar");
+        }
+        usuarioRepository.save(usuario);
+    }
 }
