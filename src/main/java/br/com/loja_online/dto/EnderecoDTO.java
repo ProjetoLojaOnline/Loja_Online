b1c2523b51249fce129b9d40c1ef9ef9fce096e5
dave <dehpacifico@gmail.com>
2025-10-20 20:32:11 -0300

    criando validacao da entidade endereco e adicionando exceptions globais para controle de erros

--- src/main/java/br/com/loja_online/dto/EnderecoDTO.java
+++ src/main/java/br/com/loja_online/dto/EnderecoDTO.java
@@ -2,23 +2,27 @@ package br.com.loja_online.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record EnderecoDTO(
-     @NotBlank
+     @NotBlank(message = "O logradouro não pode estar vazio.")
     String logradouro,
+    @NotNull(message = "O número não pode ser nulo.")
     @Min(1)
     Integer numero,
-     @NotBlank
+     @NotBlank(message = "O bairro não pode estar vazio.")
     String bairro,
-     @NotBlank
+     @NotBlank(message = "O complemento não pode estar vazio.")
     String complemento,
-     @NotBlank
+     @NotBlank(message = "A referência não pode estar vazia.")
      String referencia,
-     @NotBlank
+     @NotBlank(message = "O CEP não pode estar vazio.")
+     @Pattern(regexp = "\\d{5}-\\d{3}", message = "O CEP deve estar no formato 12345-678.")
     String cep,
-     @NotBlank
+     @NotBlank(message = "A cidade não pode estar vazia.")
     String cidade,
-     @NotBlank
+     @NotBlank(message = "O estado não pode estar vazio.")
     String estado
 ) {
 }
--- src/main/java/br/com/loja_online/services/EnderecoService.java
+++ src/main/java/br/com/loja_online/services/EnderecoService.java
@@ -2,6 +2,7 @@ package br.com.loja_online.services;
 
 import br.com.loja_online.model.Endereco;
 import br.com.loja_online.repositorios.EnderecoRepository;
+import br.com.loja_online.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +18,10 @@ public class EnderecoService {
   @Transactional
   public Endereco criarEndereco(Endereco endereco){
     return enderecoRepository.save(endereco);
+  }
+
+  public Endereco getEnderecoPorId(Integer id) {
+    return enderecoRepository.findById(id).orElseThrow(() ->
+            new ObjectNotFoundException("Endereço não encontrado com o ID: " + id));
   }
 }
